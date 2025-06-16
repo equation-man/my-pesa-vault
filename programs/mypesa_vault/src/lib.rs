41e0318bf9277af01df3ebcbf2a86f1f2fec2bef
equation-man <bigtechguyz@gmail.com>
2025-06-16 17:56:08 +0300

    MyPesa  Debit and credit vault

--- programs/mypesa_vault/src/lib.rs
+++ programs/mypesa_vault/src/lib.rs
@@ -1,6 +1,8 @@
 #![allow(unexpected_cfgs)]
 use anchor_lang::prelude::*;
 use anchor_lang::system_program::{transfer, Transfer};
+//use solana_client::nonblocking::rpc_client::RpcClient;
+//use solana_sdk::{ commitment_config::CommitmentCongig };
 
 // MyPesa modules
 mod mypesa_accounts;
@@ -17,16 +19,21 @@ pub mod mypesa_vault {
     }
 
     pub fn deposit_to_vault(ctx: Context<MypesaVaultActions>, deposit_amount: u64) -> Result<()> {
-        require!(deposit_amount>=0, MyPesaVaultError::InvalidAmount);
+        require_gt!(deposit_amount, 0, MyPesaVaultError::InvalidAmount);
 
-        msg!("Depositing {} amount to MyPesa vault", deposit_amount);
+        msg!("CREDIT: {} deposited to your MyPesa vault", deposit_amount);
+        let trans_log = &mut ctx.accounts.mypesa_vault_account_pda;
+        let clock = Clock::get().unwrap();
+        trans_log.amount = deposit_amount;
+        trans_log.updated_at = clock.unix_timestamp;
+        trans_log.trans_type = "CREDIT".to_string();
+
         transfer(
             CpiContext::new(
                 ctx.accounts.system_program.to_account_info(),
                 Transfer {
                     from: ctx.accounts.sender_wallet_token_account.to_account_info(),
                     to: ctx.accounts.mypesa_vault.to_account_info(),
-                    authority: ctx.accounts.sender_wallet_token_account.to_account_info(),
                 }
             ),
             deposit_amount,
@@ -35,18 +42,24 @@ pub mod mypesa_vault {
     }
 
     pub fn withdraw_from_vault(ctx: Context<MypesaVaultActions>, withdraw_amount: u64) -> Result<()> {
-        require_gt!(amount, ctx.accounts.mypesa_vault.to_account_info(), MyPesaVaultError::WithdrawLimit);
+        require_gt!(withdraw_amount, ctx.accounts.mypesa_vault.to_account_info().lamports(), MyPesaVaultError::WithdrawLimit);
 
-        msg!("Withdrawing {} amount from MyPesa vault", withdraw_amount);
+        msg!("DEBIT: {} withdrawn from your MyPesa vault", withdraw_amount);
+        let trans_log = &mut ctx.accounts.mypesa_vault_account_pda;
+        let clock = Clock::get().unwrap();
+        trans_log.amount = withdraw_amount;
+        trans_log.updated_at = clock.unix_timestamp;
+        trans_log.trans_type = "DEBIT".to_string();
+
+
         let mint_keys = ctx.accounts.mint_of_the_token_being_sent.key();
-        let signers = &[b"mypesa_vault", mint_keys.as_ref(), &ctx.bumps.mypesa_vault];
+        let signers = &[b"mypesa_vault", mint_keys.as_ref(), &[ctx.bumps.mypesa_vault]];
         transfer(
             CpiContext::new_with_signer(
                 ctx.accounts.system_program.to_account_info(),
                 Transfer {
                     from: ctx.accounts.mypesa_vault.to_account_info(),
                     to: ctx.accounts.sender_wallet_token_account.to_account_info(),
-                    authority: ctx.accounts.mypesa_vault.to_account_info(),
                 },
                 &[&signers[..]],
             ),
--- programs/mypesa_vault/src/mypesa_accounts.rs
+++ programs/mypesa_vault/src/mypesa_accounts.rs
@@ -4,17 +4,17 @@ use anchor_spl::token::{TokenAccount, Mint, Token};
 
 #[derive(Accounts)]
 pub struct InitializeMypesaVault<'info> {
-    /// CHECK: no check here, we are passing here ourselves.
+    /// CHECK: We are passing here ourselves.
     // This pda signs the transactions for the Vault.
     // Since pdas are owned by the program.
     #[account(
         init,
         payer=signer,
-        seeds=[b"mypesa_vault_account_pda"],
+        seeds=[b"mypesa_vault_account_pda", mypesa_vault.key().as_ref()],
         bump,
-        space=8
+        space=TransactionLog::INIT_SPACE,
     )]
-    mypesa_vault_account_pda: AccountInfo<'info>,
+    pub mypesa_vault_account_pda: Account<'info, TransactionLog>,
     // Holds the tokens being stored in the vault
     // Owned by the program via pda.
     #[account(
@@ -25,27 +25,26 @@ pub struct InitializeMypesaVault<'info> {
         token::mint=mint_of_the_token_being_sent,
         token::authority=mypesa_vault_account_pda,
     )]
-    mypesa_vault: Account<'info, TokenAccount>,
+    pub mypesa_vault: Account<'info, TokenAccount>,
     // Mint for the token sent to be stored in the vault.
-    mint_of_the_token_being_sent: Account<'info, Mint>,
+    pub mint_of_the_token_being_sent: Account<'info, Mint>,
 
     // Signer
     #[account(mut)]
-    signer: Signer<'info>,
-    system_program: Program<'info, System>,
-    token_program: Program<'info, Token>,
-    rent: Sysvar<'info, Rent>,
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
 }
 
 #[derive(Accounts)]
 pub struct MypesaVaultActions<'info> {
-    /// CHECK: No check, we are passing here ourselves
+    /// CHECK: we are passing here ourselves
     #[account(
         mut,
-        seeds=[b"mypesa_vault_account_pda"],
+        seeds=[b"mypesa_vault_account_pda", mypesa_vault.key().as_ref()],
         bump
     )]
-    mypesa_vault_account_pda: AccountInfo<'info>,
+    pub mypesa_vault_account_pda: Account<'info, TransactionLog>,
     #[account(
         mut,
         seeds=[b"mypesa_vault", mint_of_the_token_being_sent.key().as_ref()],
@@ -53,19 +52,28 @@ pub struct MypesaVaultActions<'info> {
         token::mint=mint_of_the_token_being_sent,
         token::authority=mypesa_vault_account_pda,
     )]
-    mypesa_vault: Account<'info, TokenAccount>,
+    pub mypesa_vault: Account<'info, TokenAccount>,
 
     // Token account sending tokens to the vault.
     #[account(mut)]
-    sender_wallet_token_account: Account<'info, TokenAccount>,
+    pub sender_wallet_token_account: Account<'info, TokenAccount>,
 
-    mint_of_the_token_being_sent: Account<'info, Mint>,
+    pub mint_of_the_token_being_sent: Account<'info, Mint>,
 
     #[account(mut)]
-    signer: Signer<'info>,
-    system_program: Program<'info, System>,
-    token_program: Program<'info, Token>,
-    rent: Sysvar<'info, Rent>,
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+}
+
+/// Transaction logs or history.
+#[account]
+#[derive(InitSpace)]
+pub struct TransactionLog {
+    pub amount: u64, // Amount transacted.
+    pub updated_at: i64, // Timestamp for the transaction time.
+    #[max_len(6)]
+    pub trans_type: String, // Transaction type
 }
 
 // PROGRAM ERRORS
@@ -76,4 +84,3 @@ pub enum MyPesaVaultError {
     #[msg("You have insuffiecient balance to perform the withdrawal")]
     WithdrawLimit,
 }
-
